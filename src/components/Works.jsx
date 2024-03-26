e0cc2487c85b7ec405b98fa361bd5be4fd47f870
Satyam750 <satyamthakur05@gmail.com>
2024-03-26 22:02:19 +0530

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,7 +23,7 @@ const ProjectCard = ({
    
 <div >
 <div 
-        className="h-52 md:h-72 md:w-[23vw] w-[85vw] lg:h-72 lg:w-[22vw] xl:w-[20vw] rounded-t-xl relative group"
+        className="h-52 md:h-72 md:w-[23vw] w-[85vw] lg:h-72 lg:w-[22vw] xl:w-[19vw] 2xl:w-[15vw] rounded-t-xl relative group"
         style={{background:`url(${image.src})`,  backgroundSize:"cover",objectFit:"contain", backgroundRepeat:"no-repeat",}}
         >
             <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
